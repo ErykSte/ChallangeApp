cdae2efdcfc57a3a337cfdedc1748c631f59ec92
ErykSte <erczeslaw97@wp.pl>
2023-12-16 09:26:30 +0100

    Zadanie Domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzień 4
+
+using System.ComponentModel.Design;
+
+var Name = "Andrzej";
+var Sex = "Mężczyzna";
+var Age = 17;
+
+if (Sex == "Kobieta" && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (Name == "Ewa" && Age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+if (Sex == "Mężczyzna" && Age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+if (!(Age == 17 && Sex == "Mężczyzna" && Name == "Andrzej"))
+{
+    Console.WriteLine();
+}
