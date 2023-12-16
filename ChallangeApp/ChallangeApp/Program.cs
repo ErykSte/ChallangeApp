297e84b262cd85d19eb1a4c3771a8c07aebf7370
ErykSte <erczeslaw97@wp.pl>
2023-12-16 11:06:04 +0100

    Zadanie domowe dzień 4.

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,22 +1,18 @@
 ﻿// Zadanie domowe dzień 4
 
-var Name = "Andrzej";
-var Sex = "Mężczyzna";
-var Age = 17;
+var name = "Ewa";
+var sex = "Kobieta";
+var age = 17;
 
-if (Sex == "Kobieta" && Age < 30)
+if (sex == "Kobieta" && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-if (Name == "Ewa" && Age == 30)
+else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-if (Sex == "Mężczyzna" && Age < 18)
+else if (sex != "Kobieta" && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
-}
-if (!(Age == 17 && Sex == "Mężczyzna" && Name == "Andrzej"))
-{
-    Console.WriteLine();
 }
