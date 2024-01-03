36063624464883c66cc43c4eb192a6f4effc42b3
ErykSte <erczeslaw97@wp.pl>
2024-01-03 10:09:42 +0100

    Zadanie domowe dzien7.

--- ChallangeApp/ChallangeApp.Tests/UserTests.cs
+++ ChallangeApp/ChallangeApp.Tests/UserTests.cs
@@ -4,18 +4,36 @@ namespace ChallangeApp.Tests
     public class UserTests
     {
         [Test]
-        public void WhenTwoNumberAdded_ShouldReturnSum()
+        public void WhenUserCollectTwoScoreAdded_ShouldCorrectResult()
         {
             //arrange
-            var user = new User("Marek", "Ja³owski", 23);
-            user.AddScore(5);
-            user.AddScore(6);
+            var user1 = new User("Marek", "Ja³owski", 23);
+            user1.AddScore(18);
+            user1.AddScore(9);
+            user1.AddScore(-5);
+            user1.AddScore(-20);
+
+            var user2 = new User("Damian", "Wiœniewski", 28);
+            user2.AddScore(3);
+            user2.AddScore(20);
+            user2.AddScore(7);
+            user2.AddScore(-16);
+
+            var user3 = new User("Aneta", "Kaczka", 35);
+            user3.AddScore(5);
+            user3.AddScore(25);
+            user3.AddScore(-4);
+            user3.AddScore(-19);
 
             //act
-            var result = user.Result;
+            var result1 = user1.Result;
+            var result2 = user2.Result;
+            var result3 = user3.Result;
 
             //assert
-            Assert.AreEqual(11, result);
+            Assert.AreEqual(2, result1);
+            Assert.AreEqual(14, result2);
+            Assert.AreEqual(7, result3);
 
         }
     }
